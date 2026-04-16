876b1b7c159d11e2f78158ba47ea62d36fcf083b
jabulani-mayenda <mayendajabulani@gmail.com>
2026-04-16 21:05:06 +0200

    made changes to document structure

---
+++ .history/phase1/DATA TYPES_20260416191335.js
@@ -0,0 +1,22 @@
+//exploring javascript data types 
+let numbertype=42;
+let stringtype="hello world"
+let boolentype=true;
+let undefined type;
+let nulltype=null;
+let objecttype={name :"john"}
+let arraytype=[1,2,3];
+let functiontype=function(){return "hello "}
+
+console.log("number:",typeof numbertype );
+
+console.log("string:",typeof stringtype );
+console.log("boolen:",typeof boolentype );
+console.log(" undefined :",typeof  undefined);
+console.log("null :",typeof  nulltype);
+console.log("object :",typeof  objecttype);
+console.log("array :",typeof  arraytype);
+console.log(" function :",typeof  functiontype);
+
+
+
---
+++ .history/phase1/string operations_20260416192430.js
@@ -0,0 +1,6 @@
+//working with strings
+let firstname="jabulani";
+let lastname ="maayenda";
+
+//concantenation with +
+let fullname1=firstname+" "+ 
